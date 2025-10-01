696361aaa959c6d0d474aabc5619a47244d4d876
juriafacil-user <juriafacil@gmail.com>
2025-10-01 16:13:23 -0400

    Update users.py

--- app/routes/users.py
+++ app/routes/users.py
@@ -5,5 +5,9 @@ router = APIRouter()
 
 @router.get("/")
 def list_users():
-    users = list(db.users.find({}, {"_id": 0}))
-    return {"users": users}
+    try:
+        users = list(db.users.find({}, {"_id": 0}))
+        return {"users": users}
+    except Exception as e:
+        return {"error": str(e)}
+
