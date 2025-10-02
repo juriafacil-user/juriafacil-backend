6efa1dcbc2d5fa494096184e1f27359ba2879a9e
juriafacil-user <juriafacil@gmail.com>
2025-10-02 12:03:31 -0400

    Update models.py

--- app/models.py
+++ app/models.py
@@ -1,1 +1,12 @@
+from bson import ObjectId
+
+def user_entity(user) -> dict:
+    return {
+        "id": str(user["_id"]),
+        "name": user["name"],
+        "email": user["email"]
+    }
+
+def users_entity(users) -> list:
+    return [user_entity(user) for user in users]
 
