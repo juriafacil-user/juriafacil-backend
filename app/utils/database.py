6ed62f97e210b24dd7e5bd9699a9de064ee6103c
juriafacil-user <juriafacil@gmail.com>
2025-10-07 16:24:49 -0400

    Update database.py

--- app/utils/database.py
+++ app/utils/database.py
@@ -1,1 +1,7 @@
+from motor.motor_asyncio import AsyncIOMotorClient
+import os
 
+MONGODB_URI = os.getenv("MONGODB_URI")
+
+client = AsyncIOMotorClient(MONGODB_URI)
+db = client["juriafacil"]  # nome do banco
