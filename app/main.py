6270ecc76936db00c55dc99dd32246203c328485
juriafacil-user <juriafacil@gmail.com>
2025-10-07 14:08:40 -0400

    Update main.py

--- app/main.py
+++ app/main.py
@@ -1,11 +1,9 @@
 from fastapi import FastAPI
-from app.routes import users
+from app.routes import users, auth
 
 app = FastAPI(title="API JuriFacil 🚀")
 
 # Registrar rotas
 app.include_router(users.router)
+app.include_router(auth.router)
 
-@app.get("/")
-def root():
-    return {"message": "API JuriFacil funcionando 🚀"}
