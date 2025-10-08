ba3b596440304373cc27cbd8e9059b7714c1a911
juriafacil-user <juriafacil@gmail.com>
2025-10-07 22:51:45 -0400

    Delete app/database.py

--- app/database.py
+++
@@ -1,7 +0,0 @@
-from motor.motor_asyncio import AsyncIOMotorClient
-import os
-
-MONGODB_URI = os.getenv("MONGODB_URI")
-
-client = AsyncIOMotorClient(MONGODB_URI)
-db = client["juriafacil"]  # nome do banco
