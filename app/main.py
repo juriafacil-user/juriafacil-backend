40fada3c6ab646532cc777105a903e97c6aea4e6
juriafacil-user <juriafacil@gmail.com>
2025-10-08 20:10:15 -0400

    Update main.py

--- app/main.py
+++ app/main.py
@@ -1,6 +1,6 @@
 from fastapi import FastAPI
 from app.routes import users, auth, whatsapp, billing
-from app.webhooks.mercadopago import router as mercadopago_router
+from app.webhooks import mercadopago
 
 app = FastAPI(title="API JuriFacil 🚀")
 
@@ -9,7 +9,7 @@ app.include_router(users.router)
 app.include_router(auth.router)
 app.include_router(whatsapp.router)
 app.include_router(billing.router)
-app.include_router(mercadopago_router, tags=["MercadoPago"])
+app.include_router(mercadopago.router)
 
 @app.get("/")
 def root():
